5889078c760696b64a7600e957296ce8c8b83238
crongle <gasmazk73@gmail.com>
2021-02-05 13:07:48 +0000

    Add double negative handling and improve order of operation.

--- calc.js
+++ calc.js
@@ -28,34 +28,39 @@ function backspace() {
 
 // Attempt to perform calculation on stack, reducing it to a single value and update the display.
 function calculate() {
-  // Double press of equals sets stack value to result of calculation
+  // Double press of equals sets stack value to result of calculation.
   if (virtualStack.length>0) {
-    stack = [...virtualStack]
+    stack = [...virtualStack] // Finalise calculation with result of calculation on virtualStack.
     clearVirtualStack()
     return
   }
-  virtualStack = stack.join('').split(/([+\-*\/])/) // Split virtualStack on operands.
-
-  let operators = ['*','/','+','-'] // Operators in order of operations.
+  // Generate formatted virtual stack.
+  virtualStack = [0, '+', ...stack] // Start all calculations with 0+ to handle leading negatives.
+    .join('')
+    .replace(/\-{2}/g, '+') // Reduce double negatvies into single positive operator.
+    .split(/([+\-*\/])/) // Split virtualStack on operands. Note: minus(-) is left out purposely to create negative numbers as needed.
+  // Perform subcalculations.
+  let operators = ['*', '/', '-', '+'] // Operators in order of desired operation.
   const operations = { // Arrow functions for each of the 4 arithmatic calculations.
     '*' : (a,b) => a*b,
     '/' : (a,b) => a/b,
-    '+' : (a,b) => a+b,
     '-' : (a,b) => a-b,
+    '+' : (a,b) => a+b
   }
   while (virtualStack.length>1) { // Calculation is finished when length is reduced to one.
     while (virtualStack.includes(operators[0])) { // Perform calculations progressively, in line with order of operations.
-      let i = virtualStack.indexOf(operators[0]) // Index of operator.
-      let result = Number(operations[operators[0]](+virtualStack[i-1],+virtualStack[i+1]).toFixed(10)) // Result of subcalculation. toFixed to address floating point decimal oddities. Number to clear trailing zeros.
+      console.log(virtualStack)
+      let i = virtualStack.indexOf(operators[0]) // Index of current operator.
+      let result = Number(operations[operators[0]](+virtualStack[i-1],+virtualStack[i+1]).toFixed(8)) // Result of subcalculation. toFixed to address floating point decimal oddities. Number to clear trailing zeros.
       virtualStack.splice(i-1, 3, result) // Reduce 2 operands and 1 operator, leaving result of the subcalculation.
     }
     operators.shift() // Move onto next operator once all operations needed with current operator have been handled.
   }
-
+  // Update display with result of calculation on virtual stack.
   updateDisplay(virtualStack)
 }
 
-// Update the calculator visual display with a presentable version of the current stack.
+// Update the calculator visual display with whatever is supplied OR presentable version of the current stack.
 function updateDisplay(supplied) {
   document.getElementById('display').textContent = supplied || stack.join('').replace(/\*/g, 'x') // Replace * with x for readability.
 }
